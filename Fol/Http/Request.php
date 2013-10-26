1a381bfa1ba41acffed8d9aaf114c7d2eee09bd9
Óscar Otero <oom@oscarotero.com>
2013-10-26 22:08:17 +0200

    Eliminadas referencias a FOL_VERSION

--- Fol/Http/Request.php
+++ Fol/Http/Request.php
@@ -104,7 +104,7 @@ class Request {
 			'SERVER_NAME' => 'localhost',
 			'SERVER_PORT' => 80,
 			'HTTP_HOST' => 'localhost',
-			'HTTP_USER_AGENT' => 'FOL/'.FOL_VERSION,
+			'HTTP_USER_AGENT' => 'FOL',
 			'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 			'HTTP_ACCEPT_LANGUAGE' => 'gl-es,es,en;q=0.5',
 			'HTTP_ACCEPT_CHARSET' => 'utf-8;q=0.7,*;q=0.7',
