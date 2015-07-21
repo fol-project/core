7ab7d2a249dad265bc15447a723b0015427b2d5d
oscarotero <oom@oscarotero.com>
2015-07-21 12:46:02 +0200

    engadido un arquivo de helpers coa función 'env()'

---
+++ src/helpers.php
@@ -0,0 +1,27 @@
+<?php
+if (!function_exists('env')) {
+    /**
+     * Gets the value of an environment variable
+     * Converts the type of values like "true", "false" or "null"
+     * 
+     * @param string $name The value name
+     * 
+     * @return mixed
+     */
+    function env($name) {
+        $value = getenv($name);
+
+        switch (strtolower($value)) {
+            case 'true':
+                return true;
+
+            case 'false':
+                return false;
+
+            case 'null':
+                return;
+        }
+
+        return $value;
+    }
+}
